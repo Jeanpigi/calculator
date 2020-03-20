3368a5a5b8247e8718f66999ea1f38bc5a46eb0c
Jean Pierre <stick-master666@hotmail.com>
2020-03-19 20:54:12 -0500

    four commit

--- calculator.go
+++ calculator.go
@@ -9,14 +9,16 @@ import (
 	"strings"
 )
 
-type calc struct{}
+//Calc es un struct importante
+type Calc struct{}
 
-func (calc) parseString(operator string) (int, error) {
+func (Calc) parseString(operator string) (int, error) {
 	result, err := strconv.Atoi(operator)
 	return result, err
 }
 
-func (c calc) operate(input string, operation string) (int, error) {
+//Operate es una funcion importante
+func (c Calc) Operate(input string, operation string) (int, error) {
 	cleanInput := strings.Split(input, operation)
 	first, err := c.parseString(cleanInput[0])
 	if err != nil {
@@ -61,7 +63,7 @@ func ReadInput() string {
 }
 
 func processResult(input string, operator string) {
-	c := calc{}
+	c := Calc{}
 	value, err := c.operate(input, operator)
 	if err != nil {
 		fmt.Println(err)
